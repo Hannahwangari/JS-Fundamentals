949ad4d19e6759cbfaf70b79cab98314f083e863
ANNABEL <annwanja950@gmail.com>
2025-05-24 16:35:07 -0500

    Add 9-add.js: print sum of two integers

---
+++ 9-add.js
@@ -0,0 +1,10 @@
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 === undefined) {
+  console.log("No argument");
+} else if (arg2 === undefined) {
+  console.log(arg1);
+} else {
+  console.log(`${arg1} ${arg2}`);
+}
